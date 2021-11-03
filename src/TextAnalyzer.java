154d34e106a748da75c4e9a406a56d3749495572
Jen <jmiller283@mail.valenciacollege.edu>
2021-11-03 19:58:16 -0400

    add map

--- src/TextAnalyzer.java
+++ src/TextAnalyzer.java
@@ -1,4 +1,26 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class TextAnalyzer {
+	public static void main(String args[]) {
+		Map<String, Integer> wordMap = buildWordMap("C:/users/David/documents/MacBeth.txt");
+		List<Entry<String, Integer>> list = sortByValueInDecreasingOrder(wordMap);
+		System.out.println("List of repeated words");
+		for (Map.Entry<String, Integer> entry : list) {
+			if (entry.getValue() > 1) {
+				System.out.println(entry.getKey() + " => " + entry.getValue());
+			}
+		}
+	}
 
+	private static List<Entry<String, Integer>> sortByValueInDecreasingOrder(Map<String, Integer> wordMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Map<String, Integer> buildWordMap(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
